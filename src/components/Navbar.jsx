2876e7f11720cb4a8ba57c8cee499adf7a1bee00
Robson47 <clonederobson@gmail.com>
2025-07-29 20:59:32 -0300

    remocao do icone do instagram

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import logo from '../assets/logo.png'
 import { FaLinkedin } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa'
-import { FaInstagram } from 'react-icons/fa'
 import { motion } from 'framer-motion'
 
 const Navbar = () => {
@@ -19,9 +18,8 @@ const Navbar = () => {
                 whileInView={{ opacity: 1, y: 10 }}
                 transition={{ duration: 0.5, delay: 0.5 }}
                 className='mg-8 flex items-center justify-center gap-4 text-3xl'>
-                <a href='#' target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+                <a href='https://www.linkedin.com/in/robson-luciano-57b47832a/' target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
                 <a href='https://www.github.com/Robson47' target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-                <a href='#' target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
             </motion.div>
         </nav>
     )
